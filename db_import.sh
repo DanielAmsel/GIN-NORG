d3be07b7c8b657ce8e3c28f3f62fe31454243ba5
Kemarv <dermarvinkeller@outlook.de>
2023-04-06 16:00:59 +0200

    #17 import commentary updated

--- db_import.sh
+++ db_import.sh
@@ -1,11 +1,11 @@
 #     Make the script executab
-# chmod +x db_csv_import.sh
+# chmod +x db_import.sh
 
 #     Copy the script to the container:
-# docker cp db_csv_import.sh projekte_mariadb_1:/tmp/db_csv_import.sh
+# docker cp db_import.sh projekte_mariadb_1:/tmp/db_import.sh
 
 #     Run the script inside the container:
-# docker exec -i projekte_mariadb_1 bash -c "/tmp/db_csv_import.sh"
+# docker exec -i projekte_mariadb_1 bash -c "/tmp/db_import.sh"
 
 
 # Replace the placeholders with your actual values
