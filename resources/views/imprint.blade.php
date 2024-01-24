1e98f1861eea35fe4b59454163b5b21893ac71df
marvin <dermarvinkeller@outlook.de>
2024-01-24 10:46:27 +0100

    changed imprint #33

--- resources/views/imprint.blade.php
+++ resources/views/imprint.blade.php
@@ -4,19 +4,22 @@
 @section('imprint')
     <div class="container py-4">
         <div class="row align-items-md-stretch">
-                <div class="col-md-6">
-                    <div class="h-100 p-5 bg-light border rounded-3">
-                        <h1 class="display-5 fw-bold">Impressum</h1>
-                        <p class="col-md-8 fs-4">Technische Hochschule Mittelhessen<br>University of Applied Sciences<br>Fachbereich Gesundheit</p>
-                        <p>Wiesenstraße 14<br>D - 35390 Gießen<br> +49 641 309-6601<br> +49 641 309-2920<br><a href="mailto:dekanat@ges.thm.de"> dekanat@ges.thm.de</a></p>
-                    </div>
-                </div>
             <div class="col-md-6">
                 <div class="h-100 p-5 bg-light border rounded-3">
-                    <p class="col-md-8 fs-4">Fachbereich Gesundheit Besucheradresse</p>
-                    <p>Gebäude B14<br>2. und 3. Stock<br>Ostanlage 45 (am Kinopolis)<br>35390 Gießen</p>
-                    <p class="col-md-8 fs-4">Entwickler Webanwendung NORG</p>
-                    <p>Projektteam bestehend aus Studierenden des Fachbereichs Gesundheit.</p>
+                    <h1 class="display-6 fw-bold">Impressum</h1>
+                    <div class="col-md-8">
+                        <p class="fs-4">Institut für Neuropathologie<br>der Justus-Liebig Universität/UGKM<br>in Gießen</p>
+                        <p class="fs-5">Arndtstr. 16<br>D - 35392 Gießen<br> +49 641/99-41181<br><a href="mailto:neuropathologie@uk-gm.de">Sekretariat</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="h-100 p-5 bg-light border rounded-3">
+                    <div class="col-md-8">
+                        <p class="fs-4">Webanwendung NORG:</p>
+                        <p class="fs-5">Ein Kooperationsprojekt des In silico Labor der Neuropathologie und des Fachbereichs Gesundheit, Technische Hochschule Mittelhessen.</p>
+                        <p class="fs-5">Für Anfragen, Unterstützung und Kooperationsmöglichkeiten wenden Sie sich bitte an <br> <a href="mailto:daniel.amsel@patho.med.uni-giessen.de">daniel.amsel@patho.med.uni-giessen.de</a></p>
+                    </div>
                 </div>
             </div>
         </div>
